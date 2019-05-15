d45bdff73d4fadc415c6586b28b2b29cf9b6747b
Fa11en <fa11en@bk.ru>
2019-05-15 16:37:29 +0300

    I think it's over.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,10 +1,7 @@
-
-
-
 public class Calculator {
     private final int loanAmount;
     private final int years;
-    public static final int MONTH =12;
+    public static final int MONTHS =12;
     private final double monthlyInterestRate;
     private int extraMonthlyPayment;
 
@@ -12,11 +9,56 @@ public class Calculator {
         this.loanAmount = loanAmount;
         this.years = numberOfYears;
         this.extraMonthlyPayment = extraMonthlyPayment;
-        this.monthlyInterestRate = interestRatePerYear / MONTH;
+        this.monthlyInterestRate = interestRatePerYear / MONTHS;
     }
-    public static void CalcAndPrint(){
+    void calculateAndPrint() {
+        double payment = calcMonthlyPayment(loanAmount, monthlyInterestRate);
+        double monthStartBalance = loanAmount;
+        double interestAmount = 0.0d;
+        int periodCount = 0;
+        double monthEndBalance = monthStartBalance;
+        System.out.println("MONTH,  STARING_BALANCE,  PAYMENT,  INTEREST,  PRINCIPAL,  ENDING_BALANCE,  TOTAL_INTEREST");
+        double monthlyPrincipal = 0.0d;
+        while (periodCount < years * MONTHS) {
+            periodCount++;
+            if (monthEndBalance >= payment) {
+                double monthlyInterest = calcMonthlyInterest(monthStartBalance, monthlyInterestRate);
+                monthlyPrincipal = calcMonthlyPrincipal(payment, extraMonthlyPayment, monthlyInterest);
+                monthEndBalance = calcMonthEndBalance(monthStartBalance, monthlyPrincipal);
+                interestAmount += monthlyInterest;
+                System.out.printf("%d, %15.2f, %12.2f, %8.2f, %8.2f, %15.2f, %15.2f%n", periodCount, monthStartBalance,
+                        payment+extraMonthlyPayment, monthlyInterest, monthlyPrincipal, monthEndBalance, interestAmount);
+                monthStartBalance = monthEndBalance;
+                if ((monthlyPrincipal + extraMonthlyPayment) > monthEndBalance){
+                    extraMonthlyPayment = 0;
+                }
+            } else {
+                System.out.printf("%d, %15.2f, %12.2f, %8.2f, %8.2f, %15.2f, %15.2f%n", periodCount, monthStartBalance,
+                        monthEndBalance, 0.0, monthEndBalance, 0.0, interestAmount);
+                break;
+            }
+        }
+    }
 
+    /**
+     * P = (Pv*R) / [1 - (1 + R)^(-n)]
+     */
+    double calcMonthlyPayment(int loanAmount, double monthlyInterestRate) {
+        return loanAmount * monthlyInterestRate / (1 - Math.pow((1 + monthlyInterestRate), -1 * years * MONTHS));
+    }
 
+    private double calcMonthlyInterest(double startingBalance, double monthlyInterestRate) {
+        return startingBalance * monthlyInterestRate;
+    }
+
+    private double calcMonthlyPrincipal(double payment, double extraMonthlyPayment, double interestAmount) {
+        return payment + extraMonthlyPayment - interestAmount;
+    }
+
+    private double calcMonthEndBalance(double startingBalance, double principal) {
+        return startingBalance - principal;
     }
 
 }
+
+
--- src/calcFeatures.java
+++ src/calcFeatures.java
@@ -1,2 +1,50 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class calcFeatures {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите размер кредита: ");
+        int loanAmount = scanner.nextInt();
+        System.out.println("Введите срок кредита в годах: ");
+        int numberOfYears = scanner.nextInt();
+        System.out.println("Введите проценты: в месяц ");
+        double annualInterestRate = scanner.nextDouble();
+        System.out.println("Введите доход: ");
+        int totalIncome = scanner.nextInt();
+        System.out.println("Введите количество членов семьи: ");
+        int numberOfFamilyMembers = scanner.nextInt();
+        int extraPayment = 100;
+
+        Calculator calculator = new Calculator(loanAmount, numberOfYears, annualInterestRate, extraPayment);
+        double monthlyPayment = calculator.calcMonthlyPayment(loanAmount, annualInterestRate / 12);
+        System.out.println("Monthly payment: "+monthlyPayment);
+        double incomeRatio;
+        switch (numberOfFamilyMembers) {
+            case 1:
+                incomeRatio = 0.50;
+                break;
+            case 2:
+                incomeRatio = 0.45;
+                break;
+            case 3:
+                incomeRatio = 0.35;
+                break;
+            case 4:
+                incomeRatio = 0.30;
+                break;
+            case 5:
+                incomeRatio = 0.25;
+                break;
+            default:
+                incomeRatio = 0.0;
+        }
+        if (monthlyPayment / totalIncome < incomeRatio) {
+            System.out.println("Кредит одобрен. Ваш  граффик платежей: ");
+            calculator.calculateAndPrint();
+        } else {
+            System.out.println("К сожалению, Вам отказано в кредите.");
+        }
+    }
 }
+
